f4e640b194c56ac3f5df7234510ae41aeb2c57b8
virginia <briosovirginia@gmail.com>
2021-05-04 10:24:38 -0300

    clona Repo function

---
+++ gitLib.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# A simple script with a function...
+
+clonaRepo()
+{
+  REPO=$1
+  USER=$2
+  BRANCH=$3
+  git clone -b $BRANCH https://github.com/$USER/$REPO.git
+
+}
+
+###
+# Main body of script starts here
+###
+echo "Testing..."
+clonaRepo teste-SCM virginiabrioso main
